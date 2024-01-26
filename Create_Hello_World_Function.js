750f78c9b2d645b58f72a7d79a2780d44cfd9b60
arise-aizen-404 <maheshavvaru2004@gmail.com>
2024-01-26 15:06:49 +0530

    Day -1 js problem

---
+++ Create_Hello_World_Function.js
@@ -0,0 +1,19 @@
+// Function Syntax
+
+var createHelloWorld = function () {
+  return function (...args) {
+    return "Hello World";
+  };
+};
+
+// Arrow Syntax
+
+var createHelloWorld = function () {
+  return () => "Hello World";
+};
+
+// Arrow Syntax + Rest Arguments
+
+var createHelloWorld = function () {
+  return (...args) => "Hello World";
+};
