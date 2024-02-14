a5981bd1a000244e19906c42c8e718f74ca8620c
arise-aizen-404 <maheshavvaru2004@gmail.com>
2024-02-14 14:13:12 +0530

    Day -16 js problem

--- Chunk_Array.js
+++ Chunk_Array.js
@@ -1,23 +1,11 @@
 /**
  * @param {Array} arr
  * @param {number} size
- * @return {Array[]}
+ * @return {Array}
  */
-var chunk = function(arr, size) {
-    const chunkedArray = [];
-    let index = 0;
-    
-    while (index < arr.length) {
-        let count = size;
-        const temp = [];
-        
-        while (count-- > 0 && index < arr.length) {
-        temp.push(arr[index]);
-        index++;
-        }
-        
-        chunkedArray.push(temp);
-    }
-    
-    return chunkedArray;
+var chunk = function (arr, size) {
+  const chunkedArray = [];
+  for (let index = 0; index < arr.length; index += size)
+    chunkedArray.push(arr.slice(index, index + size));
+  return chunkedArray;
 };
